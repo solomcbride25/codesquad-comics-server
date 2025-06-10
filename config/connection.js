b190fb4fff14f52f384c1fa9ae091a42f3babd0f
solomcbride25 <solomcbride22@gmail.com>
2025-06-09 20:28:34 -0400

    mongo database

---
+++ config/connection.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose")
+
+async function main() {
+    try {
+        await mongoose.connect(process.env.DB_URL); 
+        console.log('MongoDB database is connected.')
+    } catch (error) {
+        console.error(`There was an error connecting to the database:${error}`)
+    }
+}
