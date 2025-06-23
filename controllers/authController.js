f5cd1ab44c868c9d25c1aa1131b4f4d06ae5a214
solomcbride25 <solomcbride22@gmail.com>
2025-06-23 10:58:30 -0400

    fixed err

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
-const passport = require('passport')
-const bcrypt = require('bcrypt');
-const { request } = require('express');
+const passport = require("passport");
+const bcrypt = require("bcrypt");
+const { request } = require("express");
 
 const register = async (req, res, next) => {
   const { firstName, lastName, username, password } = req.body;
@@ -29,35 +29,33 @@ const login = async (req, res, next) => {
 };
 
 const loginLocal = (req, res, next) => {
-  passport.authenticate("local", async (err, user, info)) 
+  passport.authenticate("local", async(err, user, info));
   try {
-    if (error) return next (err);
-    
+    if (error) return next(err);
+
     if (!user) {
       return res.status(401).json({
         message: info?.message || "Authentication failed",
         statusCode: 401,
       });
-     }
-     req.login (user, (err) => {
-      if (err) return next (err);
+    }
+    req.login(user, (err) => {
+      if (err) return next(err);
 
       const userCopy = user.toObject();
       userCopy.password = undefined;
 
-  return res.status(200).json({
-    message: "Login successful",
-    statusCode: 200,
-    data: {user: userCopy},
-  });
-});
-
+      return res.status(200).json({
+        message: "Login successful",
+        statusCode: 200,
+        data: { user: userCopy },
+      });
+    });
   } catch (error) {
     next(err);
-    (req,res,next);
+    req, res, next;
   }
-} 
-
+};
 
 const logout = async (req, res, next) => {
   console.log("Initializing logout controller logic...");
@@ -78,59 +76,59 @@ const logout = async (req, res, next) => {
 const logoutRequest = async (req, res, next) => {
   if (err) {
     return next(err);
-  } else {sessionDestruction();
-  res.clearCookie("connect.sid", { path: "/" });
-  res.status(200).json({
-    success: true,
-    statusCode: 200
-  })
-}
-}
+  } else {
+    sessionDestruction();
+    res.clearCookie("connect.sid", { path: "/" });
+    res.status(200).json({
+      success: true,
+      statusCode: 200,
+    });
+  }
+};
 
 const signupRequest = async (req, res, next) => {
-  const {firstName, lastName, username, password, googleId, githubId} = req.body;
+  const { firstName, lastName, username, password, googleId, githubId } =
+    req.body;
   if (!firstName || !username || !password) {
     return res.status(400).json({
       message: "First name, username, and password are required.",
       statusCode: 400,
-    })
-  }};;
-  
-  try {
-    const hashedPassword = async (req, res, next) => { await bcrypt.hash(password, 10);
-    const newUser = new User ({
+    });
+  }
+
+try {
+  const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
       firstName,
       lastName,
       username,
       password: hashedPassword,
       googleId,
       githubId,
-    })
-  }
-  
-const newUser = async (req, res, next) => {
-await newUser.save();
-  req.login(newUser, (err) => {
-    if (err) return next(err);
-  })
-};
+    });
 
-  const userCopy = newUser.toObject();
-  userCopy.password = undefined;
-
-  return res.status(201).json({
-    message: "Signup successful",
-    statusCode: 201,
-    data: {user: userCopy},
+  await newUser.save();
+    req.login(newUser, (err) => {
+      if (err) return next(err);
+      const userCopy = newUser.toObject();
+      userCopy.password = undefined;
+    
+      return res.status(201).json({
+        message: "Signup successful",
+        statusCode: 201,
+        data: { user: userCopy },
+    });
   });
+
 } catch (err) {
   next(err);
 }
+};
 
 module.exports = {
   login,
   register,
   logout,
   loginLocal,
-  signupRequest
-}
+  signupRequest,
+};
